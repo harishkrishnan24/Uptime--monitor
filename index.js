0c69d5b9a6713f51861562765daebbeff7b3b345
harishkrishnan24 <harishkrishnan1993@gmail.com>
2018-03-29 21:25:03 +0530

    Added checks API

--- index.js
+++ index.js
@@ -110,5 +110,7 @@ var unifiedServer = function (req, res) {
 // Define a request router
 var router = {
     'ping': handlers.ping,
-    'users': handlers.users
+    'users': handlers.users,
+    'tokens': handlers.tokens,
+    'checks': handlers.checks
 };
